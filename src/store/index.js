fa961c0f044cfbcc2d01112a0f8f1bd2636aadda
Stuart Nelson <stuart@northernbadger.co.uk>
2018-05-14 11:27:47 -0700

    added vuext persit state

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,19 @@ import Vue from 'vue'
 import Vuex from 'vuex'
 import Form from '@/store/modules/form.js'
 
+// Plugins
+import VuexPersistence from 'vuex-persist'
+
 Vue.use(Vuex)
+
+// Setting up local storage
+const vuexLocal = new VuexPersistence({
+    storage: window.localStorage
+})
 
 export default new Vuex.Store({
   modules: [
       Form
-  ]
+  ],
+  plugins: [vuexLocal.plugin]
 })
